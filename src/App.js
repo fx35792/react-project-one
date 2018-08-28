ac0685553d543ab3717b8681092a6881ac318151
fx35792 <fx35792@163.com>
2018-08-28 18:15:16 +0800

    add file

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import {BrowserRouter, Route} from 'react-router-dom';
 import store from './store';
 
 import AuthRoute from './components/authRoute';
+import Dashboard from './components/Dashboard';
+
 import Login from './contianer/login';
 import Register from './contianer/register';
 import BossInfo from './contianer/BossInfo';
 import GeniusInfo from './contianer/GeniusInfo';
 
+//涉及页面   me
 class App extends Component {
     render() {
         return (
@@ -20,6 +23,7 @@ class App extends Component {
                         <Route path='/bossInfo' exact component={BossInfo}></Route>
                         <Route path='/login' exact component={Login}></Route>
                         <Route path='/register' exact component={Register}></Route>
+                        <Route component={Dashboard}></Route>
                     </div>
                 </BrowserRouter>
             </Provider>
